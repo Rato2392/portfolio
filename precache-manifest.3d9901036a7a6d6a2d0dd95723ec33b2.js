0b6b78016ecf9d9f95aa788a00dbcc2a65d07492
Rato2392 <joao.r.rato@gmail.com>
2020-11-25 10:02:23 +0000

    Updates

---
+++ precache-manifest.3d9901036a7a6d6a2d0dd95723ec33b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c155af4dd8d95adcc61b6153cc6e0a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75682fd05ea84fa0c27b",
+    "url": "/portfolio/static/css/main.ca02ffc7.chunk.css"
+  },
+  {
+    "revision": "731ad79837e421afe8c3",
+    "url": "/portfolio/static/js/2.7166b4e1.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/portfolio/static/js/2.7166b4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75682fd05ea84fa0c27b",
+    "url": "/portfolio/static/js/main.473037fc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1bd52c2bcead9ebe1c1d3d0156e1d3b0",
+    "url": "/portfolio/static/media/appbar_1.1bd52c2b.png"
+  },
+  {
+    "revision": "524186f41f84549b4b0702b386ff39a9",
+    "url": "/portfolio/static/media/araragi.524186f4.png"
+  },
+  {
+    "revision": "ace2ec02021fe4cbba2498758cb7ba63",
+    "url": "/portfolio/static/media/back6.ace2ec02.jpg"
+  },
+  {
+    "revision": "af9badc51775befaa08ffb0ab9945175",
+    "url": "/portfolio/static/media/header_img.af9badc5.jpg"
+  }
+]);
